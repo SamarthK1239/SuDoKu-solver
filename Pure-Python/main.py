a3963dd6c2d2f068fb8e73905e3e6351819ab648
Samarth Kulkarni <kulkarni.samarth@yahoo.com>
2023-09-15 19:08:33 -0400

    Added CSP Solver Basics
    Created a CSP Solver and added some constraints. Each individual 3x3 square needs special constraints, so that still needs to be coded in (and optimized)

--- Pure-Python/main.py
+++ Pure-Python/main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+from constraint import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+problem = Problem()
+
+cell_list = my_array = [x for x in range(80)]
+
+# Add cells and all possible values
+problem.addVariable(cell_list, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+# Add constraints
+# 1. All cells in a row must be different
+for i in range(9):
+    problem.addConstraint(AllDifferentConstraint(), [x for x in range(i*9, i*9+9)])
+
+# 2. All cells in a column must be different
+for i in range(9):
+    problem.addConstraint(AllDifferentConstraint(), [x for x in range(i, 81, 9)])
+
+# 3. All cells in a 3x3 square must be different
+for i in range(3):
+    for j in range(3):
+        problem.addConstraint(AllDifferentConstraint(), [x for x in range(i*3+j*27, i*3+j*27+3)] + [x for x in range(i*3+j*27+9, i*3+j*27+12)] + [x for x in range(i*3+j*27+18, i*3+j*27+21)])
+
+# 4. All cells in a 3x3 square must be different
+for i in range(3):
+    for j in range(3):
+        problem.addConstraint(AllDifferentConstraint(), [x for x in range(i*3+j*27+3, i*3+j*27+6)] + [x for x in range(i*3+j*27+12, i*3+j*27+15)] + [x for x in range(i*3+j*27+21, i*3+j*27+24)])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
