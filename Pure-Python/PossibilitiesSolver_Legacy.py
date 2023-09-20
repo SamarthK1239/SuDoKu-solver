64cb2d1a7f0d01b399c39a386db06a0bcdef811f
Samarth Kulkarni <kulkarni.samarth@yahoo.com>
2023-09-20 12:01:42 -0400

    Started with modification of inputs

--- Pure-Python/PossibilitiesSolver_Legacy.py
+++ Pure-Python/PossibilitiesSolver_Legacy.py
@@ -11,3 +11,34 @@ from constraint import *
 import copy
 import time
 
+input_puzzle = ("070040800"
+                "000009634"
+                "043610900"
+                "094358000"
+                "052460800"
+                "000020530"
+                "080902007"
+                "070100040"
+                "091005802")
+expected_output = "679518243543729618821634957794352186358461729216897534485276391962183475137945862"
+
+
+# The input String is the digits of the puzzle, left to right, top to bottom
+# Convert the input string to a 9x9 array
+def convert_input(input_string):
+    string = []
+    for i in range(9):
+        string.append(input_string[i*9:i*9+9])
+
+    print(string)
+    puzzle = []
+    for i in range(9):
+        puzzle.append([])
+        for j in range(9):
+            puzzle[i].append(int(string[i][j]))
+    return puzzle
+
+
+
+
+print(convert_input(input_puzzle))
